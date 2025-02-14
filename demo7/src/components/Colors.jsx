fb9fd8b2446d0b82cd26379fa128effaa21dcff9
abiyababy8 <abiyababy16@gmail.com>
2025-02-14 11:55:02 +0530

    added some code

--- demo7/src/components/Colors.jsx
+++ demo7/src/components/Colors.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function Colors() {
+    const [colors,setColors]=useState({
+        first:'Red',
+        second:'Blue',
+        third:'Orange',
+        fourth:'Green'
+    })
   return (
     <>
     <h2 style={{color:'blue'}}>Colors</h2>
+    <ul>
+        <li>{colors.first}</li>
+        <li>{colors.second}</li>
+        <li>{colors.third}</li>
+        <li>{colors.fourth}</li>
+    </ul>
+    <button onClick={()=>setColors({...colors,first:'Yellow'})}>Change First Color</button>
+    <button onClick={()=>setColors({...colors,second:"Black"})}>Change Second color</button>
     </>
   )
 }
