ba9b47518e6a4bbcb9aa2652b9a9ab9f45a81ff3
abiyababy8 <abiyababy16@gmail.com>
2025-02-17 12:03:59 +0530

    added some code

--- demo8/src/App.jsx
+++ demo8/src/App.jsx
@@ -1,16 +1,18 @@
-
+import { useState } from "react"
 
 function App() {
-
-  const loginUser = () => {
-    console.log('Inside loginUser function')
+  const [uname,setUname]=useState('')
+  const loginUser = (e) => {
+    e.preventDefault()   //to prevent default reloading of the page
+    console.log('Inside loginUser function..')
+    console.log(uname)
   }
   return (
     <>
       <h2>REACT FORMS</h2>
       <form onSubmit={loginUser}>
         <label htmlFor="">Username</label>
-        <input type="text" placeholder="Enter your username" style={{ padding: '10px', marginLeft: '10px', width: '250px', borderRadius: '5px' }} />
+        <input type="text" placeholder="Enter your username" style={{ padding: '10px', marginLeft: '10px', width: '250px', borderRadius: '5px' }} onChange={(e)=>setUname(e.target.value)}/>
         <input type="submit" style={{ backgroundColor: 'blue', padding: '10px', marginLeft: '10px', color: 'white' }} value={'Login'} />
       </form>
     </>
