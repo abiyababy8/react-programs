f9a25b6fcb455048805d5e1c767af76e06393ec0
abiyababy8 <abiyababy16@gmail.com>
2025-02-17 11:49:18 +0530

    added some code

--- demo8/src/App.jsx
+++ demo8/src/App.jsx
@@ -2,10 +2,17 @@
 
 function App() {
 
-
+  const loginUser = () => {
+    console.log('Inside loginUser function')
+  }
   return (
     <>
-      
+      <h2>REACT FORMS</h2>
+      <form onSubmit={loginUser}>
+        <label htmlFor="">Username</label>
+        <input type="text" placeholder="Enter your username" style={{ padding: '10px', marginLeft: '10px', width: '250px', borderRadius: '5px' }} />
+        <input type="submit" style={{ backgroundColor: 'blue', padding: '10px', marginLeft: '10px', color: 'white' }} value={'Login'} />
+      </form>
     </>
   )
 }
