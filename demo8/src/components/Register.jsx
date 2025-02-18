8f9c3cbc3693269bd9a93d198ab81e9abee6e9a2
abiyababy8 <abiyababy16@gmail.com>
2025-02-18 12:59:59 +0530

    added some code

--- demo8/src/components/Register.jsx
+++ demo8/src/components/Register.jsx
@@ -3,41 +3,37 @@ import React, { useState } from 'react'
 function Register() {
     const [userData, setUserData] = useState(
         {
-            uname: '', ugender: '', ucourse: ''
+            uname: '',
+            ugender: '',
+            ucourse: ''
         }
     )
     const RegisterUser = (e) => {
         e.preventDefault()
-        console.log('Inside RegisterUser function')
-
-        console.log(userData)
+        
     }
     const getUserData = (e) => {
-        // object destructuring
-        // const {key}=object_name
-
-        const { name, value } = e.target;
-        setUserData({ ...userData, [name]: value })
-        
+        console.log(e.target.value)
+        console.log(e.target.name)
     }
     return (
         <>
-            <h2>Registration Form</h2>
+            <h2 style={{ color: 'blue' }}>Registration Form</h2>
             <form onSubmit={RegisterUser}>
-                <label htmlFor="" style={{ fontSize: '20px' }}>First Name</label>
-                <input type="text" style={{ padding: '10px', marginLeft: '10px', width: '250px', borderRadius: '5px' }} placeholder='Enter your First Name' name='uname' onChange={(e) => getUserData(e)} />
+                <label htmlFor=""style={{ fontSize: '20px' }}>First Name:</label>
+                <input type="text" onChange={(e) => getUserData(e)}  style={{ padding: '10px', marginLeft: '10px', width: '250px', borderRadius: '5px' }} placeholder='Enter your First Name' name='uname' />
                 <div style={{ marginTop: '20px' }}>
-                    <label htmlFor="" style={{ fontSize: '20px' }}>Gender</label>
+                    <label htmlFor="" style={{ fontSize: '20px' }}>Gender:</label>
                     <input type="radio" name='ugender' style={{ marginLeft: '20px' }} />
                     <label htmlFor="">Male</label>
-                    <input type="radio" name='ugender'/>
+                    <input type="radio" name='ugender' />
                     <label htmlFor="">Female</label>
-                    <input type="radio" name='ugender'/>
+                    <input type="radio" name='ugender' />
                     <label htmlFor="">Other</label>
                 </div>
                 <div style={{ marginTop: '20px' }}>
-                    <label htmlFor="" style={{ fontSize: '20px' }}>Course</label>
-                    <select  id="" style={{ marginLeft: '20px' }}>
+                    <label htmlFor="" style={{ fontSize: '20px' }}>Course:</label>
+                    <select id="" style={{ marginLeft: '20px' }}>
                         <option value="CE">CE</option>
                         <option value="ME">ME</option>
                         <option value="EEE">EEE</option>
--- demo8/src/components/SimpleForm.jsx
+++ demo8/src/components/SimpleForm.jsx
@@ -14,15 +14,15 @@ function SimpleForm() {
             <form onSubmit={handleRegister}>
                 <div style={{ marginTop: '20px' }}>
                     <label htmlFor="">First Name:</label>
-                    <input type="text" onChange={(e) => setFirstName(e.target.value)} style={{ padding: '10px', marginLeft: '41px', width: '250px', borderRadius: '5px' }} />
+                    <input type="text" onChange={(e) => setFirstName(e.target.value)} style={{ padding: '10px', marginLeft: '41px', width: '250px', borderRadius: '5px' }} placeholder='Enter your First Name'/>
                 </div>
                 <div style={{ marginTop: '20px' }}>
                     <label htmlFor="">Email:</label>
-                    <input type="text" onChange={(e) => setemail(e.target.value)} style={{ padding: '10px', marginLeft: '75px', width: '250px', borderRadius: '5px' }} />
+                    <input type="text" onChange={(e) => setemail(e.target.value)} style={{ padding: '10px', marginLeft: '75px', width: '250px', borderRadius: '5px' }} placeholder='Enter your Email'/>
                 </div>
                 <div style={{ marginTop: '20px' }}>
                     <label htmlFor="">Mobile Number:</label>
-                    <input type="text" onChange={(e) => setMobNo(e.target.value)} style={{ padding: '10px', marginLeft: '10px', width: '250px', borderRadius: '5px' }} />
+                    <input type="text" onChange={(e) => setMobNo(e.target.value)} style={{ padding: '10px', marginLeft: '10px', width: '250px', borderRadius: '5px' }} placeholder='Enter your Mobile Number'/>
                 </div>
                 <div style={{ marginTop: '20px' }}>
                     <button type='submit' style={{ padding: '20px', marginLeft: '304px', color: 'white', backgroundColor: 'blue', borderRadius: '5px' }}>Submit</button>
