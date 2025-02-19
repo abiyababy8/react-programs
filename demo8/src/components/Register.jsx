b1334cf02d42a597c60e12d299564f269377fe57
abiyababy8 <abiyababy16@gmail.com>
2025-02-19 11:53:04 +0530

    added some code

--- demo8/src/components/Register.jsx
+++ demo8/src/components/Register.jsx
@@ -35,18 +35,18 @@ function Register() {
                 <div style={{ marginTop: '20px' }}>
                     <label htmlFor="" style={{ fontSize: '20px' }}>Course:</label>
                     <select id="" name='ucourse' onChange={(e) => getUserData(e)} style={{ marginLeft: '44px', width: '273px', padding: '10px', borderRadius: '5px' }}>
-                        <option selected>- Select an option -</option>
+                        <option selected disabled hidden>- Select a Course -</option>
                         <option value="CE">CE</option>
                         <option value="ME">ME</option>
                         <option value="EEE">EEE</option>
+                        <option value="CSE">CSE</option>
+                        <option value="ECE">ECE</option>
                     </select>
                     <div>
                         <input type="submit" style={{ backgroundColor: 'blue', padding: '10px', marginLeft: '299px', color: 'white', marginTop: '20px' }} value={'Register'} />
                     </div>
 
                 </div>
-
-
             </form>
         </>
     )
