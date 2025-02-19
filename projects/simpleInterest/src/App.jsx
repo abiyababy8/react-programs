cae0a82f107b092f385eb94eb4b7ccef0dcc1da3
abiyababy8 <abiyababy16@gmail.com>
2025-02-19 12:27:24 +0530

    added some code

--- projects/simpleInterest/src/App.jsx
+++ projects/simpleInterest/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={{ backgroundColor: 'black', height: '100vh' }} className="d-flex justify-content-center align-items-center">
+        <div style={{ backgroundColor: 'white', width: '500px' }} className="rounded p-5">
+          <h2 className="text-center">Simple Interest</h2>
+          <p className="text-center">Calculate Your Simple Interest Easily</p>
+          <div style={{ height: '150px', backgroundColor: 'orange' }} className="d-flex justify-content-center align-items-center flex-column mt-3 rounded shadow">
+            <h2 className="fw-bold">₹ 4000</h2>
+            <p className="fw-bold">Total Simple Interest</p>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
-
 export default App
