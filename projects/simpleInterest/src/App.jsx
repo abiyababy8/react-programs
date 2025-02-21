6cb74591dc5ef28868da8c74f6dddccabb8fc432
abiyababy8 <abiyababy16@gmail.com>
2025-02-21 12:27:41 +0530

    added some code

--- projects/simpleInterest/src/App.jsx
+++ projects/simpleInterest/src/App.jsx
@@ -2,9 +2,9 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
 function App() {
-  const [principle, setPrinciple] = useState(0);
-  const [rate, setRate] = useState(0);
-  const [year, setYear] = useState(0);
+  const [principle, setPrinciple] = useState('');
+  const [rate, setRate] = useState('');
+  const [year, setYear] = useState('');
   const [interest, setInterest] = useState(0);
 
   //states for checking if values are valid
@@ -26,7 +26,7 @@ function App() {
         setIsRateValid(true)
         setRate(input_value)
       }
-      else if (input_field === 'num_of_years') {
+      else {
         setIsYearValid(true)
         setYear(input_value)
       }
@@ -40,11 +40,28 @@ function App() {
         setIsRateValid(false)
         setRate(input_value)
       }
-      else if (input_field === 'num_of_years') {
+      else {
         setIsYearValid(false)
         setYear(input_value)
       }
     }
+  }
+  const handleCalculate = (e) => {
+    e.preventDefault();
+    if (principle === '' || rate === '' || year === '') {
+      alert("Please Enter All Values");
+    }
+    else {
+      const result = (principle * rate * year) / 100;
+      setInterest(result)
+
+    }
+  }
+  const handleReset = () => {
+    setInterest(0)
+    setPrinciple('')
+    setRate('')
+    setYear('')
   }
   return (
     <>
@@ -53,34 +70,34 @@ function App() {
           <h2 className="text-center">Simple Interest</h2>
           <p className="text-center">Calculate Your Simple Interest Easily</p>
           <div style={{ height: '150px', backgroundColor: 'orange' }} className="d-flex justify-content-center align-items-center flex-column mt-3 rounded shadow">
-            <h2 className="fw-bold">₹ 4000</h2>
+            <h2 className="fw-bold">₹ {interest}</h2>
             <p className="fw-bold">Total Simple Interest</p>
           </div>
-          <form action="" className='mt-4'>
+          <form action="" className='mt-4' onSubmit={handleCalculate}>
             <div className='mb-3'>
-              <TextField id="outlined-basic" label="Principle" variant="outlined" className='w-100' name='principle_amount' onChange={(e) => validate(e)} />
+              <TextField id="outlined-basic" label="Principle" variant="outlined" className='w-100' name='principle_amount' onChange={(e) => validate(e)} value={principle} />
               {
                 !isPrincipleValid &&
                 <p style={{ color: 'red' }}>Invalid input</p>
               }
             </div>
             <div className='mb-3'>
-              <TextField id="outlined-basic" label="Rate of Interest" variant="outlined" className='w-100' name='rate_of_interest' onChange={(e) => validate(e)} />
+              <TextField id="outlined-basic" label="Rate of Interest" variant="outlined" className='w-100' name='rate_of_interest' onChange={(e) => validate(e)} value={rate} />
               {
                 !isRateValid &&
                 <p style={{ color: 'red' }}>Invalid input</p>
               }
             </div>
             <div className='mb-3'>
-              <TextField id="outlined-basic" label="Number of Years" variant="outlined" className='w-100' name='num_of_years' onChange={(e) => validate(e)} />
+              <TextField id="outlined-basic" label="Number of Years" variant="outlined" className='w-100' name='num_of_years' onChange={(e) => validate(e)} value={year} />
               {
                 !isYearValid &&
                 <p style={{ color: 'red' }}>Invalid input</p>
               }
             </div>
             <div className='d-flex justify-content-between mb-3'>
-              <Button variant="contained" color='success' style={{ width: '190px', padding: '10px' }} disabled={!isPrincipleValid || !isRateValid || !isYearValid} >Calculate</Button>
-              <Button variant="outlined" style={{ width: '190px', padding: '10px' }}>Reset</Button>
+              <Button type='submit' variant="contained" color='success' style={{ width: '190px', padding: '10px' }} disabled={!isPrincipleValid || !isRateValid || !isYearValid} >Calculate</Button>
+              <Button variant="outlined" style={{ width: '190px', padding: '10px' }} onClick={handleReset}>Reset</Button>
             </div>
           </form>
         </div>
