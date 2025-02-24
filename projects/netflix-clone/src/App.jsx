320bb4843304732e1ec58ff42265074a3cbdc5ab
abiyababy8 <abiyababy16@gmail.com>
2025-02-24 12:58:56 +0530

    added some code

--- projects/netflix-clone/src/App.jsx
+++ projects/netflix-clone/src/App.jsx
@@ -1,19 +1,18 @@
 import Banner from "./components/Banner"
 import Nav from "./components/Nav"
 import Row from "./components/Row"
-
+import requests from "./requests"
 function App() {
   return (
     <>
-      netflix clone
       <Nav />
-      <Banner />
+      <Banner fetchurl={requests.fetchNetflixOriginals}/>
       <Row title={'Trending'}/>
       <Row title={'Netflix Originals'}/>
       <Row title={'Top Rated'}/>
       <Row title={'Action Movies'}/>
       <Row title={'Comedy Movies'}/>
-      <Row title={'Horror Movie'}/>
+      <Row title={'Horror Movies'}/>
       <Row title={'Romance Movies'}/>
       <Row title={'Documentaries'}/>
      
