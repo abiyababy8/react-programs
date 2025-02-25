2f88f2377ea128a465a16fa1f7a37aba2cc3d20a
abiyababy8 <abiyababy16@gmail.com>
2025-02-25 13:07:03 +0530

    added some code

--- projects/netflix-clone/src/components/Banner.jsx
+++ projects/netflix-clone/src/components/Banner.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import instance from '../instance'
 
-function Banner() {
+function Banner({ fetchurl }) {
+  console.log(fetchurl)
+  const image_base_url = "https://image.tmdb.org/t/p/original/";
+  const [movies, setmovies] = useState()
+  const fetchData = async () => {
+    const {data} = await instance.get(fetchurl)
+    console.log('api result:')
+    console.log(data)
+    setmovies(data.results[0]) 
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
   return (
     <>
-    <h2 style={{color:'red'}}>Banner</h2>
+      <h2 style={{ color: 'red' }}>Banner</h2>
+<div style={{backgroundImage:`url(${image_base_url}${movies?.backdrop_path})`}}></div>
     </>
   )
 }
