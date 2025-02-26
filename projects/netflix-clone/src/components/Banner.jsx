7156b181c2c702f4fd8e955330882b2503658c56
abiyababy8 <abiyababy16@gmail.com>
2025-02-26 12:57:34 +0530

    added some code

--- projects/netflix-clone/src/components/Banner.jsx
+++ projects/netflix-clone/src/components/Banner.jsx
@@ -1,23 +1,30 @@
 import React, { useEffect, useState } from 'react'
 import instance from '../instance'
+import'./banner.css'
 
 function Banner({ fetchurl }) {
   console.log(fetchurl)
   const image_base_url = "https://image.tmdb.org/t/p/original/";
-  const [movies, setmovies] = useState()
+  const [movies, setmovies] = useState({})
   const fetchData = async () => {
-    const {data} = await instance.get(fetchurl)
-    console.log('api result:')
-    console.log(data)
-    setmovies(data.results[0]) 
+    const { data } = await instance.get(fetchurl)
+    const res = data.results[Math.floor(data.results.length * Math.random())]
+    setmovies(res)
   }
   useEffect(() => {
-    fetchData()
+    setInterval(() => {
+      fetchData()
+    }, 5000);
   }, [])
   return (
     <>
-      <h2 style={{ color: 'red' }}>Banner</h2>
-<div style={{backgroundImage:`url(${image_base_url}${movies?.backdrop_path})`}}></div>
+      <div style={{ backgroundImage: `url(${image_base_url}${movies?.backdrop_path})`, height: '500px', backgroundSize: '100% 100%' }}>
+        <div className='banner_content'>
+          <h2 style={{ color: 'white' }}>{movies?.name}</h2>
+          <button className='btn btn-danger'>Play <i className="fa-solid fa-play ms-2"></i></button>
+          <button className='btn btn-outline-light ms-2'>More Info <i className="fa-solid fa-caret-down ms-2"></i></button>
+        </div>
+      </div>
     </>
   )
 }
