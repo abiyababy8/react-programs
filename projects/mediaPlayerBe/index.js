ae08c5dc013637d1e66b299978ecc527ac125d03
abiyababy8 <abiyababy16@gmail.com>
2025-03-06 12:49:32 +0530

    added some code

---
+++ projects/mediaPlayerBe/index.js
@@ -0,0 +1,26 @@
+//creating JSON server
+
+// 1) import JSON server library
+const jsonServer = require('json-server')
+
+// 2) create json server using create() method
+const mediaPlayerServer = jsonServer.create()
+
+// 3) connect jsonServer with db.json
+const router = jsonServer.router('db.json')
+
+// 4) create a middleware that process request and response
+const middleware = jsonServer.defaults()
+
+// 5) use middleware 
+mediaPlayerServer.use(middleware)
+
+// 6) use router
+mediaPlayerServer.use(router)
+
+const PORT = 5000;
+ 
+// running the mediaPlayerServer
+mediaPlayerServer.listen(PORT,()=>{
+    console.log("mediaPlayerServer is running in",PORT)
+})
